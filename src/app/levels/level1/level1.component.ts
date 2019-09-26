9dd6fa61b218ef9105ef75dab8567c1a1b30829f
Vorstoffel <bianca.vorstoffel@n-ergie.de>
2019-09-26 11:05:37 +0200

    level 1 update

--- src/app/levels/level1/level1.component.ts
+++ src/app/levels/level1/level1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Inventory, InventoryService } from 'src/app/inventory.service';
 
 @Component({
   selector: 'app-level1',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level1.component.scss']
 })
 export class Level1Component {
+
+  inventory = false;
+  constructor(itemService: InventoryService) {
+    if (itemService.getInventory() != null) {
+      this.inventory = true;
+    }
+  }
 
   attack() {
 
